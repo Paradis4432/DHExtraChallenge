1177a88a3481a7bfce8b56225f34b1fb59c6fb7c
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-11-09 14:30:31 -0300

    removed extra files

--- test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.delete('/canciones/:id', function (req, res) {
-    res.send('delete working');
-    console.log(req.params.id);
-  })
-
-module.exports = router;
